ebeaf4b462dfc1281b5ae4c57ea9ea0d9264cc8f
rkt1234 <tiwari.ravikant2001@gmail.com>
2023-12-03 23:00:06 +0530

    completed api

--- build rest api/index.js
+++ build rest api/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const users=require('./MOCK_DATA.json')
+const fs=require('fs')
 
 const app = express()
 const PORT=8000
+
+// Middleware plugin
+app.use(express.urlencoded({extended:false}))
 
 //ROUTES
 
@@ -25,9 +29,13 @@ app.get("/api/users/:id",(req,res)=>{
     return res.json(user)
 })
 
-app.post('api/users',(req,res)=>{
+app.post('/api/users',(req,res)=>{
     //to  do
-    return res.json({status:'pending'})
+    const body=req.body
+    users.push({...body,id:users.length+1})
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+        return res.json({status:"success",id:users.length})
+    })
 })
 app.patch("/api/users/:id", (req, res) => {
     //to  do
